93290caa889b2fb7a86090abf6448ec86238bf5a
NiccoloN <niccolo.nicolosi@mail.polimi.it>
2022-04-05 15:54:20 +0200

    add duplicate exceptions to the methods of SchoolDashboard

--- src/main/java/it/polimi/ingsw2022/eriantys/model/board/SchoolDashboard.java
+++ src/main/java/it/polimi/ingsw2022/eriantys/model/board/SchoolDashboard.java
@@ -16,40 +16,49 @@ import java.util.Map;
  */
 public class SchoolDashboard {
 
+    /**
+     * The player associated to this school
+     */
     public final Player player;
+
     private final List<ColoredPawn> entranceStudents;
     private final Map<PawnColor, List<ColoredPawn>> studentsTables;
     private final List<ColoredPawn> professors;
+    private final int maxTowers;
     private int towers;
 
     /**
-     * Constructs a school associated to the given player
+     * Constructs a school associated to the given player, with a maximum number of towers
      * @param player the player to associate this school with
      */
-    public SchoolDashboard(Player player) {
+    public SchoolDashboard(Player player, int maxTowers) {
 
         this.player = player;
         entranceStudents = new ArrayList<>();
         studentsTables = new HashMap<>();
         for (PawnColor color : PawnColor.values()) studentsTables.put(color, new ArrayList<>());
         professors = new ArrayList<>();
+        this.maxTowers = maxTowers;
+        towers = maxTowers;
     }
 
     /**
      * Place a colored pawn at the entrance of this school. Only pawns that represent students should be placed at the entrance
      * @param student the student to place
      * @throws RuntimeException if the entrance of this school already contains 7 or more students
+     * @throws RuntimeException if the given student is already at the entrance of this school
      */
     public void addToEntrance(ColoredPawn student) {
 
         if (entranceStudents.size() >= 7) throw new RuntimeException("Maximum number of students at the entrance already reached");
+        if (entranceStudents.contains(student)) throw new RuntimeException("No duplicates allowed");
         entranceStudents.add(student);
     }
 
     /**
      * Removes the given student from the entrance of this school, if present
      * @param student the student to remove
-     * @return the removed student for chaining
+     * @return the removed student
      */
     public ColoredPawn removeFromEntrance(ColoredPawn student) {
 
@@ -57,17 +66,20 @@ public class SchoolDashboard {
     }
 
     /**
-     * Places the given student at the table of the corresponding color
+     * Places a colored pawn at the table of the corresponding color. Only pawns that represent students should be placed with this method
      * @param student the student to place
      * @return the coins obtained by placing the student
      * @throws RuntimeException if the table of the corresponding color already contains 9 or more students
+     * @throws RuntimeException if the given student is already at the table of its color
      */
     public int addToTable(ColoredPawn student) {
 
         List<ColoredPawn> table = studentsTables.get(student.color);
         if (table.size() >= 9) throw new RuntimeException("Maximum number of students at the " + student.color + " table already reached");
+        if (table.contains(student)) throw new RuntimeException("No duplicates allowed");
         table.add(student);
-        return 0; //TODO classe separata per table
+
+        return table.size() % 3 == 0 ? 1 : 0;
     }
 
     /**
@@ -89,5 +101,46 @@ public class SchoolDashboard {
     public boolean containsProfessor(PawnColor color) {
 
         return professors.stream().anyMatch((x) -> x.color == color);
+    }
+
+    /**
+     * Place a colored pawn into this school. Only pawns that represent professors should be place with this method
+     * @param professor the professor to place
+     * @throws RuntimeException if the given professor is already in this school
+     */
+    public void addProfessor(ColoredPawn professor) {
+
+        if (professors.contains(professor)) throw new RuntimeException("No duplicates allowed");
+        professors.add(professor);
+    }
+
+    /**
+     * Removes the professor of the given color from this school
+     * @param color the color of the professor to remove
+     * @return the removed professor
+     */
+    public ColoredPawn removeProfessor(PawnColor color) {
+
+        return professors.stream().filter((x) -> x.color == color).findAny().orElseThrow();
+    }
+
+    /**
+     * Places a tower in this school
+     * @throws RuntimeException if the number of towers in this school is already at its maximum
+     */
+    public void addTower() {
+
+        if (towers >= maxTowers) throw new RuntimeException("Cannot add anymore towers");
+        towers++;
+    }
+
+    /**
+     * Removes a tower from this school
+     * @throws RuntimeException if the number of towers in this school is 0
+     */
+    public void removeTower() {
+
+        if (towers <= 0) throw new RuntimeException("There are no towers in this school");
+        towers--;
     }
 }
