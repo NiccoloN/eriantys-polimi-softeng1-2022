456bb15745483c8a7ee9cbd100a85611f19a7e82
Niccolò Nicolosi <niccolo.nicolosi@mail.polimi.it>
2022-04-06 16:44:04 +0200

    add CardFactory class add helpers.json file imported gson library

---
+++ src/main/java/it/polimi/ingsw2022/eriantys/model/cards/CardFactory.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw2022.eriantys.model.cards;
+
+import com.google.gson.Gson;
+import it.polimi.ingsw2022.eriantys.model.players.Mage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CardFactory {
+
+    private static final Gson gson = new Gson();
+    private static final String helpersFilePath = "resources/helpers.json";
+    private static HelperCard[] helperCardSamples;
+
+    /**
+     * Reads the json file containing helpers' data and stores card samples
+     * @throws IOException if helperFilePath is not a valid path or the file could not be opened
+     */
+    private static void readHelpersFile() throws IOException {
+
+        Path path = Path.of(helpersFilePath);
+        String json = Files.readString(path);
+        helperCardSamples = gson.fromJson(json, HelperCard[].class);
+    }
+
+    /**
+     * @param index the index of the requested helper card
+     * @param mage the mage associated with the requested helper card
+     * @return a new instance of the requested helper card, specified by the given index and mage.
+     * @throws IOException if
+     */
+    public static HelperCard createHelperCard(int index, Mage mage) throws IOException {
+
+        if (helperCardSamples == null) readHelpersFile();
+
+        HelperCard sample = helperCardSamples[index - 1];
+        return new HelperCard(sample.index, sample.priority, sample.movement, mage);
+    }
+}
