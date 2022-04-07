fbb30452ef3ae10c34b99f06eacf1d23c682c9e5
Niccolò Nicolosi <niccolo.nicolosi@mail.polimi.it>
2022-04-07 14:04:34 +0200

    add character cards factory methods to CardFactory add characters.json file

--- src/main/java/it/polimi/ingsw2022/eriantys/model/cards/CardFactory.java
+++ src/main/java/it/polimi/ingsw2022/eriantys/model/cards/CardFactory.java
@@ -17,25 +17,39 @@ import java.nio.file.Path;
 public class CardFactory {
 
     private static final Gson gson = new Gson();
-    private static final String helpersFilePath = "resources/helpers.json";
+    private static final String RESOURCES_DIR = "resources";
+    private static final String HELPERS_FILE_NAME = "helpers.json";
+    private static final String CHARACTERS_FILE_NAME = "characters.json";
     private static HelperCard[] helperCardSamples;
+    private static CharacterCard[] characterCardSamples;
 
     /**
      * Reads the json file containing helpers' data and stores card samples
-     * @throws IOException if helperFilePath is not a valid path or the file could not be opened
+     * @throws IOException if the specified path is not valid or the file could not be opened
      */
     private static void readHelpersFile() throws IOException {
 
-        Path path = Path.of(helpersFilePath);
+        Path path = Path.of(RESOURCES_DIR, HELPERS_FILE_NAME);
         String json = Files.readString(path);
         helperCardSamples = gson.fromJson(json, HelperCard[].class);
     }
 
     /**
+     * Reads the json file containing characters' data and stores card samples
+     * @throws IOException if the specified path is not valid or the file could not be opened
+     */
+    private static void readCharacterFile() throws IOException {
+
+        Path path = Path.of(RESOURCES_DIR, CHARACTERS_FILE_NAME);
+        String json = Files.readString(path);
+        characterCardSamples = gson.fromJson(json, CharacterCard[].class);
+    }
+
+    /**
      * @param index the index of the requested helper card
      * @param mage the mage associated with the requested helper card
-     * @return a new instance of the requested helper card, specified by the given index and mage.
-     * @throws IOException if
+     * @return a new instance of the requested helper card, specified by the given index and mage
+     * @throws IOException if the specified path is not valid or the file could not be opened
      */
     public static HelperCard createHelperCard(int index, Mage mage) throws IOException {
 
@@ -43,5 +57,18 @@ public class CardFactory {
 
         HelperCard sample = helperCardSamples[index - 1];
         return new HelperCard(sample.index, sample.priority, sample.movement, mage);
+    }
+
+    /**
+     * @param index the index of the requested character card
+     * @return a new instance of the requested character card, specified by the given index
+     * @throws IOException if the specified path is not valid or the file could not be opened
+     */
+    public static CharacterCard createCharacterCard(int index) throws IOException {
+
+        if (characterCardSamples == null) readCharacterFile();
+
+        CharacterCard sample = characterCardSamples[index - 1];
+        return new CharacterCard(sample.index, null); //TODO skill factory
     }
 }
