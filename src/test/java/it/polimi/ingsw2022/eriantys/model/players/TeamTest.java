a6eade2465c3d634c998a6a61995a5ebe3d01bb3
NiccoloN <niccolo.nicolosi@mail.polimi.it>
2022-04-10 21:11:02 +0200

    add setUp() method to TeamTest class

--- src/test/java/it/polimi/ingsw2022/eriantys/model/players/TeamTest.java
+++ src/test/java/it/polimi/ingsw2022/eriantys/model/players/TeamTest.java
@@ -1,10 +1,19 @@
 package it.polimi.ingsw2022.eriantys.model.players;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TeamTest {
+
+    @BeforeEach
+    void setUp() {
+
+         Team.WHITE.reset();
+         Team.BLACK.reset();
+         Team.GRAY.reset();
+    }
 
     @Test
     void addPlayer_getSize() {
