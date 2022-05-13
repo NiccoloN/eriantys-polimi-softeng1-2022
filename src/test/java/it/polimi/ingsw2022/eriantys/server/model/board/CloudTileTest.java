169d2fb0c434ac4ca26d721197d5ea9581b1b601
Francesco Maccari <f.maccari@infos.it>
2022-05-13 16:41:16 +0200

    Board testing files

---
+++ src/test/java/it/polimi/ingsw2022/eriantys/server/model/board/CloudTileTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw2022.eriantys.server.model.board;
+
+import it.polimi.ingsw2022.eriantys.server.model.pawns.ColoredPawn;
+import it.polimi.ingsw2022.eriantys.server.model.pawns.PawnColor;
+import it.polimi.ingsw2022.eriantys.server.model.board.CloudTile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CloudTileTest {
+
+  private CloudTile tile;
+
+    @BeforeEach
+    void setUp() {
+      tile = new CloudTile();
+    }
+
+    @Test
+    void addStudent() {
+      final ColoredPawn student = new ColoredPawn(PawnColor.PINK);
+      assertTrue(tile.isEmpty());
+      tile.addStudent(student);
+      assertFalse(tile.isEmpty());
+      assertThrows(RuntimeException.class, () -> tile.addStudent(student));
+      tile.addStudent(new ColoredPawn(PawnColor.BLUE));
+      tile.addStudent(new ColoredPawn(PawnColor.GREEN));
+      assertThrows(RuntimeException.class, () -> tile.addStudent(new ColoredPawn(PawnColor.BLUE)));
+    }
+
+    @Test
+    void withdrawStudents() {
+      assertEquals(0, tile.withdrawStudents().size());
+      assertTrue(tile.isEmpty());
+      tile.addStudent(new ColoredPawn(PawnColor.BLUE));
+      tile.addStudent(new ColoredPawn(PawnColor.GREEN));
+      assertFalse(tile.isEmpty());
+      assertEquals(2, tile.withdrawStudents().size());
+      assertTrue(tile.isEmpty());
+    }
+
+    @Test
+    void isEmpty() {
+      assertTrue(tile.isEmpty());
+      tile.addStudent(new ColoredPawn(PawnColor.BLUE));
+      tile.addStudent(new ColoredPawn(PawnColor.GREEN));
+      assertFalse(tile.isEmpty());
+    }
+}
