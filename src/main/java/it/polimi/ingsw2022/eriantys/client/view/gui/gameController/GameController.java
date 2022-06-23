aa4500ec096959bc86e4f4b338ab7f196d011560
Francesco Maccari <f.maccari@infos.it>
2022-06-23 02:17:54 +0200

    create DashboardGUIComponent to handle schools rendering logic

--- src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/gameController/GameController.java
+++ src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/gameController/GameController.java
@@ -16,10 +16,6 @@ import java.util.ResourceBundle;
 
 public class GameController extends SceneController implements Initializable {
 
-    private final Map<PawnColor, Image> studentsImages;
-    private final Map<PawnColor, Image> professorsImages;
-    private final Image whiteTowerImage, blackTowerImage, greyTowerImage, motherNatureImage;
-
     @FXML
     GridPane entrance1, tables1, professors1, towers1, 
             entrance2, tables2, professors2, towers2,
@@ -29,22 +25,12 @@ public class GameController extends SceneController implements Initializable {
     public GameController(EriantysGUI gui) {
 
         super(gui);
-        studentsImages = new HashMap<>(5);
-        for(PawnColor color : PawnColor.values()) studentsImages.put(color, ImageFactory.getStudentImage(color));
-
-        professorsImages = new HashMap<>(5);
-        for(PawnColor color : PawnColor.values()) professorsImages.put(color, ImageFactory.getProfessorImage(color));
-
-        whiteTowerImage = ImageFactory.getWhiteTowerImage();
-        blackTowerImage = ImageFactory.getBlackTowerImage();
-        greyTowerImage = ImageFactory.getGreyTowerImage();
-        motherNatureImage = ImageFactory.getMotherNatureImage();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        Image image = studentsImages.get(PawnColor.RED);
+        Image image = ImageFactory.studentsImages.get(PawnColor.RED);
 
         for(int i = 0; i < 5; i++)
             for(int j = 0; j < 2; j++)
@@ -54,10 +40,10 @@ public class GameController extends SceneController implements Initializable {
             for(int j = 0; j < 10; j++)
                 tables1.add(new ImageView(image), j, i);
 
-        Image image1 = professorsImages.get(PawnColor.RED);
+        Image image1 = ImageFactory.professorsImages.get(PawnColor.RED);
         for(int n = 0; n < 5; n++) professors1.add(new ImageView(image1), 0, n);
 
-        Image image2 = ImageFactory.getWhiteTowerImage();
+        Image image2 = ImageFactory.whiteTowerImage;
         for(int i = 0; i < 4; i++)
             for(int j = 0; j < 2; j++)
                 towers1.add(new ImageView(image2), j, i);
