65ab753e4872ec1117515d4415c3051600c8cbc5
Francesco Maccari <f.maccari@infos.it>
2022-06-24 17:25:24 +0200

    Add towers handling logic, remove unused variables, move local variables

--- src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/gameController/components/DashboardGUIComponent.java
+++ src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/gameController/components/DashboardGUIComponent.java
@@ -14,18 +14,10 @@ import java.util.HashMap;
 
 public class DashboardGUIComponent {
 
-    private final HashMap<PawnColor, Integer> COLOR_ROW_INDEX;
-
-    private final int ENTRANCE_COLS = 2;
-    private final int ENTRANCE_ROWS = 5;
-    private final int TABLE_COLS = 10;
-    private final int TOWERS_COLS = 2;
-    private final int TOWERS_ROWS = 4;
-
-    private GridPane entrancePane;
-    private GridPane tablePane;
-    private GridPane professorPane;
-    private GridPane towersPane;
+    private final GridPane entrancePane;
+    private final GridPane tablePane;
+    private final GridPane professorPane;
+    private final GridPane towersPane;
 
     public DashboardGUIComponent(Group school) {
         this.entrancePane = (GridPane) school.getChildren().get(1);
@@ -33,12 +25,8 @@ public class DashboardGUIComponent {
         this.professorPane = (GridPane) school.getChildren().get(3);
         this.towersPane = (GridPane) school.getChildren().get(4);
 
-        // Initializing utils data structures
-        COLOR_ROW_INDEX = new HashMap<>();
-
         int colorRow = 0;
         for (PawnColor color : PawnColor.values()) {
-            COLOR_ROW_INDEX.put(color, colorRow);
 
             // Setting up professors pane
             ColoredPawnImageView professorsImageView = new ColoredPawnImageView(ImageFactory.PROFESSOR_SIZE);
@@ -47,6 +35,7 @@ public class DashboardGUIComponent {
             professorPane.add(professorsImageView, 0, colorRow);
 
             // Setting up table pane
+            int TABLE_COLS = 10;
             for (int col = 0; col < TABLE_COLS; col++) {
                 ColoredPawnImageView tableImageView = new ColoredPawnImageView(ImageFactory.STUDENT_SIZE);
                 tableImageView.setStudentOfColor(color);
@@ -57,7 +46,9 @@ public class DashboardGUIComponent {
         }
 
         // Setting up entrance pane
+        int ENTRANCE_ROWS = 5;
         for (int row = 0; row < ENTRANCE_ROWS; row ++) {
+            int ENTRANCE_COLS = 2;
             for (int col = 0; col < ENTRANCE_COLS; col++) {
                 ColoredPawnImageView coloredImageView = new ColoredPawnImageView(ImageFactory.STUDENT_SIZE);
                 coloredImageView.setVisible(false);
@@ -66,14 +57,15 @@ public class DashboardGUIComponent {
         }
 
         // Setting up towers pane
+        int TOWERS_ROWS = 4;
         for (int row = 0; row < TOWERS_ROWS; row ++) {
+            int TOWERS_COLS = 2;
             for (int col = 0; col < TOWERS_COLS; col++) {
                 ColoredPawnImageView coloredImageView = new ColoredPawnImageView(ImageFactory.TOWER_SIZE);
                 coloredImageView.setVisible(false);
                 towersPane.add(coloredImageView, col, row);
             }
         }
-
     }
 
     public void setEntranceStudents(int students, PawnColor color) {
