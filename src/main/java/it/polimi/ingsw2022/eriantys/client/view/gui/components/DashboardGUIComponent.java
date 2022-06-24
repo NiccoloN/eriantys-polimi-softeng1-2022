797c9bc130d22a42ffae92ce24bb7051be953a59
Francesco Maccari <f.maccari@infos.it>
2022-06-24 13:10:30 +0200

    finish DashboardGUIComponent

--- src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/components/DashboardGUIComponent.java
+++ src/main/java/it/polimi/ingsw2022/eriantys/client/view/gui/components/DashboardGUIComponent.java
@@ -1,123 +1,120 @@
 package it.polimi.ingsw2022.eriantys.client.view.gui.components;
 
 import it.polimi.ingsw2022.eriantys.client.view.gui.gameController.ImageFactory;
+import it.polimi.ingsw2022.eriantys.client.view.gui.utils.ColoredImageView;
 import it.polimi.ingsw2022.eriantys.server.model.pawns.PawnColor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class DashboardGUIComponent {
 
-    private final int MAX_TABLE_STUDENTS = 10;
     private final HashMap<PawnColor, Integer> COLOR_ROW_INDEX;
-    private final HashMap<PawnColor, String> COLOR_TO_ID;
 
-    GridPane entrancePane;
-    GridPane tablePane;
-    GridPane professorPane;
-    GridPane towersPane;
+    private final int ENTRANCE_COLS = 2;
+    private final int ENTRANCE_ROWS = 5;
+    private final int TABLE_COLS = 10;
 
-    private final HashMap<PawnColor, Integer> entranceStudents;
-    private final HashMap<PawnColor, Integer> tableStudents;
-    private final HashMap<PawnColor, Boolean> professors;
-    int towers;
+    private GridPane entrancePane;
+    private GridPane tablePane;
+    private GridPane professorPane;
+    private GridPane towersPane;
 
-    public DashboardGUIComponent(GridPane entrancePane, GridPane tablePane, GridPane professorsPane, GridPane towersPane) {
-        COLOR_ROW_INDEX = new HashMap<>();
-        COLOR_TO_ID = new HashMap<>();
-
+    public DashboardGUIComponent(GridPane entrancePane, GridPane tablePane, GridPane professorPane, GridPane towersPane) {
         this.entrancePane = entrancePane;
         this.tablePane = tablePane;
-        this.professorPane = professorsPane;
+        this.professorPane = professorPane;
         this.towersPane = towersPane;
 
-        entranceStudents = new HashMap<>();
-        tableStudents = new HashMap<>();
-        professors = new HashMap<>();
+        // Initializing utils data structures
+        COLOR_ROW_INDEX = new HashMap<>();
 
         int colorRow = 0;
         for (PawnColor color : PawnColor.values()) {
-            entranceStudents.put(color, 0);
-            tableStudents.put(color, 0);
-            professors.put(color, false);
             COLOR_ROW_INDEX.put(color, colorRow);
+
+            // Setting up professors pane
+            ColoredImageView professorsImageView = new ColoredImageView();
+            professorsImageView.setColor(color);
+            professorPane.add(professorsImageView, 0, colorRow);
+
+            // Setting up table pane
+            for (int col = 0; col < TABLE_COLS; col++) {
+                ColoredImageView tableImageView = new ColoredImageView();
+                tableImageView.setColor(color);
+                tablePane.add(tableImageView, col, colorRow);
+            }
             colorRow++;
         }
 
-        towers = 0;
-
-        COLOR_TO_ID.put(PawnColor.RED, "#RED");
-        COLOR_TO_ID.put(PawnColor.YELLOW, "#YELLOW");
-        COLOR_TO_ID.put(PawnColor.BLUE, "#BLUE");
-        COLOR_TO_ID.put(PawnColor.PINK, "#PINK");
-        COLOR_TO_ID.put(PawnColor.GREEN, "#GREEN");
-    }
-
-    private int[] getEmptyEntranceNode() {
-        for (Node node : entrancePane.getChildren()) {
-            if (node.getId() == null) {
-                int[] coords = {GridPane.getColumnIndex(node), GridPane.getRowIndex(node)};
-                return coords;
+        // Populating the entrance grid with ImageViews
+        for (int row = 0; row < ENTRANCE_ROWS; row ++) {
+            for (int col = 0; col < ENTRANCE_COLS; col++) {
+                ColoredImageView coloredImageView = new ColoredImageView();
+                entrancePane.add(coloredImageView, col, row);
             }
         }
-        return new int[] {-1, -1};
+
     }
 
     public void addStudentToEntrance(PawnColor color) {
-        entranceStudents.put(color, entranceStudents.get(color) + 1);
-
         Image studentImage = ImageFactory.studentsImages.get(color);
-        ImageView studentImageView = new ImageView(studentImage);
-        studentImageView.setId(COLOR_TO_ID.get(color));
-
-        int [] emptyNodeCoords = getEmptyEntranceNode();
-        entrancePane.add(studentImageView, emptyNodeCoords[0], emptyNodeCoords[1]);
+        for (Node child : entrancePane.getChildren()) {
+            ColoredImageView coloredImageView = (ColoredImageView) child;
+            if ( coloredImageView.getImage() == null) {
+                coloredImageView.setImage(ImageFactory.studentsImages.get(color));
+                coloredImageView.setColor(color);
+                return;
+            }
+        }
     }
 
     public void removeStudentFromEntrance(PawnColor color) {
-        entranceStudents.put(color, entranceStudents.get(color) - 1);
-
-        entrancePane.getChildren().remove(entrancePane.lookup(COLOR_TO_ID.get(color)));
+        for (Node child : entrancePane.getChildren()) {
+            ColoredImageView coloredImageView = (ColoredImageView) child;
+            if (coloredImageView.getColor() == color) {
+                coloredImageView.setImage(null);
+                coloredImageView.setColor(null);
+                return;
+            }
+        }
     }
 
     public void addStudentToTable(PawnColor color) {
-        if (tableStudents.get(color) < MAX_TABLE_STUDENTS) {
-            int newStudentIndex = tableStudents.get(color);
-            tableStudents.put(color, tableStudents.get(color) + 1);
-
-            Image studentImage = ImageFactory.studentsImages.get(color);
-            tablePane.add(new ImageView(studentImage), newStudentIndex, COLOR_ROW_INDEX.get(color));
-        } else {
-            throw new RuntimeException("Max number of students to a table reached");
+        for (Node child : tablePane.getChildren()) {
+            ColoredImageView coloredImageView = (ColoredImageView) child;
+            if (coloredImageView.getColor() == color && coloredImageView.getImage() == null) {
+                coloredImageView.setImage(ImageFactory.studentsImages.get(color));
+                return;
+            }
         }
     }
 
     public void addProfessor(PawnColor color) {
-        professors.put(color, true);
-
-        Image professorImage = ImageFactory.professorsImages.get(color);
-        professorPane.add(new ImageView(professorImage), 0, COLOR_ROW_INDEX.get(color));
+        for (Node child : professorPane.getChildren()) {
+            ColoredImageView coloredImageView = (ColoredImageView) child;
+            if (coloredImageView.getColor() == color) {
+                coloredImageView.setImage(ImageFactory.studentsImages.get(color));
+                return;
+            }
+        }
     }
 
     public void removeProfessor(PawnColor color) {
-        professors.remove(color, false);
-
-        professorPane.getChildren().removeIf(
-                node -> GridPane.getColumnIndex(node) == 0 &&
-                        GridPane.getRowIndex(node) == COLOR_ROW_INDEX.get(color));
+        for (Node child : professorPane.getChildren()) {
+            ColoredImageView coloredImageView = (ColoredImageView) child;
+            if (coloredImageView.getColor() == color) {
+                coloredImageView.setImage(null);
+                return;
+            }
+        }
     }
 
-    public void addTower() {
-        towers++;
-    }
+    // TODO: towers
 
-    public void removeTower() {
-        towers--;
-    }
 }
